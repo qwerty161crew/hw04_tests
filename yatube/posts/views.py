ae72554dde304fff327e40f4c244628bbb2cffbb
Алексей Матвеев <qwerty161crew@gmail.com>
2023-03-06 23:30:26 +0300

    обновил функцию

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -69,17 +69,21 @@ def post_create(request):
 
 @login_required
 def post_edit(request, post_id):
-    post = get_object_or_404(Post, id=post_id)
-    if request.user.id != post.author.id:
-        return redirect("posts:post_detail",
-                        kwargs={'post_id': post_id})
-    form = PostForm(request.POST or None, instance=post)
+    post = get_object_or_404(Post, pk=post_id)
+    if post.author != request.user:
+        return redirect('posts:post_detail', post_id=post_id)
+
+    form = PostForm(
+        request.POST or None,
+        files=request.FILES or None,
+        instance=post
+    )
     if form.is_valid():
         form.save()
-        return redirect(reverse("posts:post_detail",
-                                kwargs={'post_id': post_id}))
+        return redirect('posts:post_detail', post_id=post_id)
     context = {
-        "form": form,
-        "is_edit": True,
+        'post': post,
+        'form': form,
+        'is_edit': True,
     }
-    return render(request, "posts/create_post.html", context)
+    return render(request, 'posts/create_post.html', context)
