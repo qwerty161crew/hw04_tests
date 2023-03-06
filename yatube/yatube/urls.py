31352c197abd84034f283dc6733c6e19ef02dfc3
Алексей Матвеев <qwerty161crew@gmail.com>
2023-03-06 23:19:59 +0300

    изменения в юрл

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -11,3 +13,7 @@ urlpatterns = [
     path('auth/', include('users.urls', namespace='auth')),
     path('auth/', include('django.contrib.auth.urls')),
 ]
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
